a033e25017f736654a1b30015a943005c7919bc1
yigit <yigitkarabulut51@gmail.com>
2022-08-27 20:14:19 +0300

    My first swift app

--- todo/ViewController.swift
+++ todo/ViewController.swift
@@ -9,11 +9,60 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtNumberOne: UITextField!
+    
+    
+    @IBOutlet weak var txtNumberTwo: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    var result = Int()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnCarpiAction(_ sender: Any) {
+        
+        if let firstNumber = Int(txtNumberOne.text!){
+            if let secondNumber = Int(txtNumberTwo.text!){
+                result = firstNumber * secondNumber
+                resultLabel.text = String(result)
+            }
+        }
+        
+        
+    }
+    @IBAction func btnParseAction(_ sender: Any) {
+        if let firstNumber = Int(txtNumberOne.text!){
+            if let secondNumber = Int(txtNumberTwo.text!){
+                result = firstNumber / secondNumber
+                resultLabel.text = String(result)
+            }
+        }
+        
+        
+    }
+    @IBAction func btnPlusAction(_ sender: Any) {
+        
+        if let firstNumber = Int(txtNumberOne.text!){
+            if let secondNumber = Int(txtNumberTwo.text!){
+                result = firstNumber + secondNumber
+                resultLabel.text = String(result)
+            }
+        }
+        
+         
+    }
+    
+    @IBAction func btnMinusAction(_ sender: Any) {
+        if let firstNumber = Int(txtNumberOne.text!){
+            if let secondNumber = Int(txtNumberTwo.text!){
+                result = firstNumber * secondNumber
+                resultLabel.text = String(result)
+            }
+        }
+        
+    }
 }
 
